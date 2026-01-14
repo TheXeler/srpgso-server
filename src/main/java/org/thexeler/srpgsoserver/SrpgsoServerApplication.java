b1b21cf6cc32b229cb4ff360f7e911a72e9fb470
TheXeler <1377266215@qq.com>
2026-01-14 15:01:40 +0800

    Initial commit

---
+++ src/main/java/org/thexeler/srpgsoserver/SrpgsoServerApplication.java
@@ -0,0 +1,13 @@
+package org.thexeler.srpgsoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpgsoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpgsoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/thexeler/srpgsoserver/SrpgsoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thexeler.srpgsoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpgsoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
